a38c02bc27a68fb9b5f30b5cafd7e9a4a8d7e6e0
Gianluca Consoli <gianluca.consoli@studio.unibo.it>
2021-12-11 16:51:13 +0100

    List with a normale Filter(no lamndas)

---
+++ LabdasExperiments/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LabdasExperiments
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var list = new List<int> (new int[] { 10, 20, 30, 40 });
+            var newList = Filter(list);
+            foreach (var elem in newList) Console.Write(elem+" ");
+        }
+
+        static IList<int> Filter(IList<int> list)
+        {
+            var l = new List<int>();
+            foreach (var t in list) if (t > 0) l.Add(t);
+            return l;
+        }
+    }
+}
