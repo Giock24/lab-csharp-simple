8cec419140b6a1e54f4e67561a7768c69d40e569
Gianluca Consoli <gianluca.consoli@studio.unibo.it>
2021-12-11 17:37:50 +0100

    My First Delegate

--- LabdasExperiments/Program.cs
+++ LabdasExperiments/Program.cs
@@ -5,27 +5,21 @@ namespace LabdasExperiments
 {
     public class Program
     {
+        static bool FilterPositive(int i) => i > 0;
+
         static void Main(string[] args)
         {
             var list = new List<int> (new int[] { -10, 20, 30, 40 });
-            var newList = Filter(list, new OnlyPositive());
+            var newList = Filter(list, new IntPredicate(FilterPositive));
             foreach (var elem in newList) Console.Write(elem+" ");
         }
 
-        class OnlyPositive : IIntPredicate
-        {
-            public bool filter(int i) => i > 0;
-        }
+        public delegate bool IntPredicate(int i); // è come scrivere quel interfaccia che abbiamo eliminato
 
-        interface IIntPredicate
-        {
-            bool filter(int i);
-        }
-
-        static IList<int> Filter(IList<int> list, IIntPredicate predicate)
+        static IList<int> Filter(IList<int> list, IntPredicate predicate)
         {
             var l = new List<int>();
-            foreach (var t in list) if (predicate.filter(t)) l.Add(t);
+            foreach (var t in list) if (predicate(t)) l.Add(t);
             return l;
         }
     }
