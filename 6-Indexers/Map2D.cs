329f390fd665611628739dc1f76e3bdd2f501525
Gianluca Consoli <gianluca.consoli@studio.unibo.it>
2021-12-18 14:03:46 +0100

    Done some progress on ex06 ;)

--- 6-Indexers/Map2D.cs
+++ 6-Indexers/Map2D.cs
@@ -8,32 +8,48 @@ namespace Indexers
     /// <inheritdoc cref="IMap2D{TKey1,TKey2,TValue}" />
     public class Map2D<TKey1, TKey2, TValue> : IMap2D<TKey1, TKey2, TValue>
     {
+
+        //private TValue[][] _values; //= new TValue[3][];
+        //private int _size;
+        private readonly Dictionary<Tuple<TKey1, TKey2>, TValue> _elem2D = new Dictionary<Tuple<TKey1, TKey2>, TValue>();
+
         /// <inheritdoc cref="IMap2D{TKey1, TKey2, TValue}.NumberOfElements" />
         public int NumberOfElements
         {
-            get
-            {
-                throw new NotImplementedException();
-            }
+            get => _elem2D.Count;
         }
 
         /// <inheritdoc cref="IMap2D{TKey1, TKey2, TValue}.this" />
         public TValue this[TKey1 key1, TKey2 key2]
         {
-            get { throw new NotImplementedException(); }
-            set { throw new NotImplementedException(); }
+
+            get { return _elem2D[Tuple.Create(key1, key2)]; }
+            set { _elem2D.Add(Tuple.Create(key1, key2), value); }
         }
 
         /// <inheritdoc cref="IMap2D{TKey1, TKey2, TValue}.GetRow(TKey1)" />
         public IList<Tuple<TKey2, TValue>> GetRow(TKey1 key1)
         {
-            throw new NotImplementedException();
+            IList<Tuple<TKey2, TValue>> row = new List<Tuple<TKey2, TValue>>();
+            foreach (var elem in _elem2D)
+            {
+                if (key1.Equals(elem.Key.Item1))
+                {
+                    row.Add(Tuple.Create(elem.Key.Item2, elem.Value));
+                }
+            }
+
+            return row;
         }
 
         /// <inheritdoc cref="IMap2D{TKey1, TKey2, TValue}.GetColumn(TKey2)" />
         public IList<Tuple<TKey1, TValue>> GetColumn(TKey2 key2)
         {
-            throw new NotImplementedException();
+            /* vers. Prof */
+            return _elem2D.Keys
+                .Where(tuple => tuple.Item2.Equals(key2))
+                .Select(tuple => Tuple.Create(tuple.Item1, _elem2D[tuple]))
+                .ToList();
         }
 
         /// <inheritdoc cref="IMap2D{TKey1, TKey2, TValue}.GetElements" />
